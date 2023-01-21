3e1beb0c53de7a541195dea24c712023c4badce4
BK-SPC <96095498+BK-SPC@users.noreply.github.com>
2023-01-21 12:21:10 -0500

    fix gen script

--- gen.bat
+++ gen.bat
@@ -1,4 +1,5 @@
-START cmd.exe /k "cd Update/GenScripts/ & gen & exit"
-START cmd.exe /k "cd Source/Startup/Gen/ & gen & exit"
+START /W cmd.exe /k "cd Update/GenScripts/ & gen & exit"
+START /W cmd.exe /k "cd Source/Startup/Gen/ & gen & exit"
+
 git add .
 git commit -m "gen"
